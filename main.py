c253273880514916a17ae2e57f1f8e7903458c7a
xSelee <58655444+xSelee@users.noreply.github.com>
2023-12-02 04:11:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ bot._skip_check = lambda x, y: False
 
 async def btall():
     while True:
-        client.sendMessage(str(channelid), ".give xLoki 1000")  # Command: .bt or .bt all
+        client.sendMessage(str(channelid), ".")  # Command: .bt or .bt all
         await asyncio.sleep(1200)  # interval between each command in seconds
 
 
 async def ho():
     while True:
-        client.sendMessage(str(channelid), ".bt max")  # Command: .bt or .bt all
+        client.sendMessage(str(channelid), ".")  # Command: .bt or .bt all
         await asyncio.sleep(choice([900])) 
 
 async def cricdrop():
